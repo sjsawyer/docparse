675e72ee5f2a795b1a8c8a04a1a49a7667ef03b3
Shaun <shjsawyer@gmail.com>
2017-09-30 16:13:22 -0400

    add docstrings

--- documentparser.py
+++ documentparser.py
@@ -3,7 +3,22 @@ import re
 
 
 class DocumentParser():
-    ''' Parse class '''
+    '''
+    Parser to parse the text of almost any text file type (to that of the
+    extent of `textract`.
+
+    Parameters
+    ----------
+    query : collections.OrderedDict
+        An ordered dictionary in which the keys are the categories to parse
+        the text into, and the values are either functions to apply on the
+        text, or regex expressions to search for.
+    delimiter : string
+        A string that separates blocks of text to individually parse.
+    discard : int, optional
+        Ignore the first `discard` pieces of text after separating based on
+        `delimiter`.
+    '''
     def __init__(self, query, delimiter, discard=0):
         self._query_functions = self._set_query_functions(query)
         self._compiled_query_regex = self._compile_regex(query)
@@ -13,7 +28,7 @@ class DocumentParser():
 
     def _compile_regex(self, query):
         '''
-        convert any regex in the query dict into compiled regex patterns
+        Convert any regex in the query dict into compiled regex patterns.
         '''
         q_reg = {}
         for q in query.keys():
@@ -22,9 +37,7 @@ class DocumentParser():
         return q_reg
 
     def _set_query_functions(self, query):
-        '''
-        set the functions to apply on the text
-        '''
+        ''' Set the functions to apply on the text. '''
         q_funcs = {}
         for q in query.keys():
             if callable(query[q]):
@@ -32,6 +45,7 @@ class DocumentParser():
         return q_funcs
 
     def _parse_text(self, text):
+        ''' Parse after setting the functions and regex. '''
         parsed = {}
         # Apply functions to text
         for q in self._query_functions.keys():
@@ -44,6 +58,23 @@ class DocumentParser():
         return parsed
 
     def parse_document(self, text_document, outfile=""):
+        '''
+        Apply the initialized DocumentParser to a text document.
+
+        Parameters
+        ----------
+        text_document : string
+            Path to the document.
+        outfile : string, optional
+            File which to write the parsed data. Note the data will be written
+            in csv format.
+
+        Returns
+        -------
+        If `outfile` specified, data will be written to it in csv format.
+        Otherwise, data will be returned as a list of dictionarys.
+
+        '''
         text = textract.process(text_document)
         text_chunks = text.split(self.delimiter)
         if self.discard:
@@ -63,5 +94,5 @@ class DocumentParser():
         return outfile
 
     def __call__(self, text_document, outfile=""):
-        ''' optional call method of DocumentParser '''
+        ''' Optional call method of DocumentParser. '''
         return self.parse_document(text_document, outfile=outfile)
